cd751a627b25a3ad8c04cdcd41cb5b8fbd7d690a
Md Touhidul Islam <r.sorker9@gmail.com>
2022-08-26 00:40:29 +0600

    Project Starting

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { SearchBar } from './Components/SearchBar';
+import { Stories } from './Components/Stories';
+import { Pagination } from './Components/Pagination';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h3>Welcome to news app</h3>
+    <SearchBar />
+    <Stories />
+    <Pagination />
+    </>
   );
 }
 
